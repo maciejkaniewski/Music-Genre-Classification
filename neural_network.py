d1e1f46af5ff7d64a13b3b72a457b5b9928edb0d
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2023-05-30 11:28:55 +0200

    Refactor code

--- neural_network.py
+++ neural_network.py
@@ -21,56 +21,44 @@ class MyDataset(Dataset):
         return self.X[idx], self.y[idx]
 
 
-# comparison of hidden layers sizes
 class MyModel(pl.LightningModule):
     def __init__(self):
-        super(MyModel, self).__init__()
-        self.dense1 = nn.Linear(58, 512)
-        self.dropout1 = nn.Dropout(0.2)
-        self.dense2 = nn.Linear(512, 256)
-        self.dropout2 = nn.Dropout(0.2)
-        self.dense3 = nn.Linear(256, 128)
-        self.dropout3 = nn.Dropout(0.2)
-        self.dense4 = nn.Linear(128, 64)
-        self.dropout4 = nn.Dropout(0.2)
-        self.dense5 = nn.Linear(64, 10)
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(58, 512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(64, 10),
+            nn.Softmax(dim=1)
+        )
 
     def forward(self, x):
-        x = torch.relu(self.dense1(x))
-        x = self.dropout1(x)
-        x = torch.relu(self.dense2(x))
-        x = self.dropout2(x)
-        x = torch.relu(self.dense3(x))
-        x = self.dropout3(x)
-        x = torch.relu(self.dense4(x))
-        x = self.dropout4(x)
-        x = self.dense5(x)
-        return torch.softmax(x, dim=1)
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self(x)
-        loss = nn.CrossEntropyLoss()(y_pred, y)
-
-        pred = torch.argmax(y_pred, dim=1)
-        acc = torch.sum(pred == y).item() / len(pred)
-
-        self.log("train_loss", loss, on_epoch=True, on_step=False, prog_bar=True)
-        self.log("train_acc", acc, on_epoch=True, on_step=False, prog_bar=True)
-
-        return loss
+        return self.shared_step(batch, "train")
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self(x)
-        loss = nn.CrossEntropyLoss()(y_pred, y)
+        return self.shared_step(batch, "val")
 
+    def shared_step(self, batch, prefix):
+        x_, y_ = batch
+        y_pred = self(x_)
+        loss = nn.CrossEntropyLoss()(y_pred, y_)
         pred = torch.argmax(y_pred, dim=1)
-        acc = torch.sum(pred == y).item() / len(pred)
-
-        self.log("val_loss", loss, on_epoch=True, on_step=False, prog_bar=True)
-        self.log("val_acc", acc, on_epoch=True, on_step=False, prog_bar=True)
-
+        acc = torch.sum(pred == y_).item() / len(pred)
+        self.log(f'{prefix}_loss', loss, on_epoch=True, prog_bar=True)
+        self.log(f'{prefix}_acc', acc, on_epoch=True, prog_bar=True)
+        return loss
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters())
@@ -85,17 +73,17 @@ convertor = LabelEncoder()
 y = convertor.fit_transform(class_list)
 
 fit = StandardScaler()
-X = fit.fit_transform(np.array(final_data.iloc[:, :-1], dtype=float))
+x = fit.fit_transform(np.array(final_data.iloc[:, :-1], dtype=float))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-train_dataset = MyDataset(X_train, y_train)
-test_dataset = MyDataset(X_test, y_test)
+train_dataset = MyDataset(x_train, y_train)
+test_dataset = MyDataset(x_test, y_test)
 
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=128)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=12)
+test_loader = DataLoader(test_dataset, batch_size=8, num_workers=12)
 
 model = MyModel()
 
-trainer = pl.Trainer(max_epochs=500, enable_model_summary=True)
+trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs=500, log_every_n_steps=25)
 trainer.fit(model, train_loader, test_loader)
